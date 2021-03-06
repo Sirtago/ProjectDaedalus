8fd405569d696696a08f37e5586b137ab6a0850a
Niall Sharma <Niall.k.sharma@gmail.com>
2021-03-05 19:59:52 -0600

    BIGCOMMIT

---
+++ Assets/RoomSpawner.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomSpawner : MonoBehaviour
+{
+    public int openingDirection;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(openingDirection == 1)
+        {
+
+        }
+        else if(openingDirection == 2)
+        {
+            
+        }
+        else if(openingDirection == 3)
+        {
+            
+        }
+        else if(openingDirection == 4)
+        {
+            
+        }
+    }
+}
